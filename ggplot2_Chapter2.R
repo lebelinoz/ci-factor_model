4114dc9737f8c785a8a619032c7bf2aa6abc1db8
Alain LeBel (Gaming PC) <alain.lebel@bigpond.com>
2016-11-13 15:43:06 +1100

    continuing exercises

--- ggplot2_Chapter2.R
+++ ggplot2_Chapter2.R
@@ -4,6 +4,9 @@
 
 library(ggplot2)
 data(mpg)
+data(diamonds)
+data(economics)
+
 
 # how to know which data packages are included in ggplot2?
 data()
@@ -21,3 +24,20 @@ summary(mpg)
 
 # Section 2.3
 ggplot(mpg, aes(x = displ, y = hwy)) + geom_point()
+ggplot(mpg, aes(cty,hwy)) + geom_point()
+ggplot(mpg, aes(model, manufacturer)) + geom_point()
+ggplot(mpg, aes(manufacturer, model)) + geom_point()
+
+ggplot(mpg, aes(cty,hwy)) + geom_point()
+ggplot(diamonds, aes(carat, price)) + geom_point()
+ggplot(economics, aes(date, unemploy)) + geom_line()
+ggplot(mpg, aes(cty)) + geom_histogram()
+
+# Section 2.4
+ggplot(mpg, aes(displ, hwy, colour = class)) + geom_point()
+ggplot(mpg, aes(displ, hwy, shape = drv)) + geom_point()
+ggplot(mpg, aes(displ, hwy, size = cyl)) + geom_point()
+
+ggplot(mpg, aes(displ, hwy, colour = class, shape = drv, size = cyl)) + geom_point()
+
+ggplot(mpg, aes(displ, cty, colour=class)) + geom_point()
