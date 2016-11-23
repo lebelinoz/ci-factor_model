71df3f10332562992970f73ecfd9f2dc8e67749e
Alain LeBel (Gaming PC) <alain.lebel@bigpond.com>
2016-11-24 07:56:33 +1100

    Created ISLR

---
+++ ISLR_Exercises.R
@@ -0,0 +1,8 @@
+################################################
+## An Introduction to Statistical Learning    ##
+## (with Applications in R)                   ##
+## by James, Witten, Hastie & Tibshirani      ##
+################################################
+
+library(ISLR) # <- contains most data sets for the book
+library(MASS) # <- contains the "Boston" dataset
