46ca54b13ccae4e37ce64621e3409ddb7d428f08
Alain LeBel (Gaming PC) <alain.lebel@bigpond.com>
2016-11-24 09:18:38 +1100

    saving wip

--- ISLR_Exercises.R
+++ ISLR_Exercises.R
@@ -6,3 +6,17 @@
 
 library(ISLR) # <- contains most data sets for the book
 library(MASS) # <- contains the "Boston" dataset
+library(ggplot2)
+
+data(Wage)
+summary(Wage)
+
+# Figure 1.1a:
+ggplot(Wage, aes(age, wage)) + geom_point() + geom_smooth()
+
+# Figure 1.1b:
+ggplot(Wage, aes(year, wage)) + geom_point() # + stat_smooth() # <-- I can't get the trend line to work
+
+# Figure 1.1c:
+ggplot(Wage, aes(education, wage)) + geom_boxplot()
+
