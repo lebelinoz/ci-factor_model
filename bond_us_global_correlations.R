9e95670a45c035d76c74156645508e9d72f35500
lebelinoz <alebel@cooperinvestors.com>
2017-01-17 11:54:06 +1100

    Tweak!

--- bond_us_global_correlations.R
+++ bond_us_global_correlations.R
@@ -1,13 +1,12 @@
 ###########################################################################
-## BOND-LIKE EQUITIES IN BRUNSWICK                                       ##
+##                                                                       ##
 ##  The goal is to study the correlations between the returns of stocks  ##
-##  in the Brunswick fund vs bond indices (AU & US), in the hopes of     ##
-##  confirming or changing the current value subset classification.      ##
+##  in the Global fund vs bond indices (AU & US).                        ##
 ##                                                                       ##
 ##  Use S&P indices for the government bonds.                            ##
 ##  FactSet codes are SPBDAGVT-CME for Aus and SPBDUSB0 for US.          ##
 ##                                                                       ##
-##  AL, 19 October 2016                                                  ##
+##  AL, 17 January 2017                                                  ##
 ###########################################################################
 
 # Preamble
@@ -69,7 +68,7 @@ xts_stock_data_aud = xts_stock_data_aud[1:(nrow(xts_stock_data_aud) - 1),] # <--
 fx_rates_all = as.xts(raw_FX[, -1], order.by = raw_FX[, 1])
 fx_rates = fx_rates_all[which(index(fx_rates_all) %in% index(xts_stock_data_aud))]
 
-# Now create a USD equivalent time series
+# Now create a USD equivalent time series:
 xts_stock_data_usd = xts(apply(xts_stock_data_aud, 2, function(col) col * fx_rates), index(xts_stock_data_aud))
 
 # Now we can compute weekly and monthly returns (I need to use a function, I think)
@@ -109,82 +108,82 @@ bond_us_monthly_return = bond_us_monthly_return[which(difftime(index(bond_us_mon
 ## CALCULATIONS ##
 ##################
 
-cor_aud_monthly = cor(monthly_returns_aud, bond_au_monthly_return)
-colnames(cor_aud_monthly) = c("AU.monthly")
+#cor_aud_monthly = cor(monthly_returns_aud, bond_au_monthly_return)
+#colnames(cor_aud_monthly) = c("AU.monthly")
 
 cor_usd_monthly = cor(monthly_returns_usd, bond_us_monthly_return)
 colnames(cor_usd_monthly) = c("US.monthly")
 write.zoo(cor_usd_monthly, paste("C://Temp//", "USD monthly correlations to US Bond Index - ", fund_code, ".csv", sep = ""), row.names = TRUE)
 
-cor_aud_weekly = cor(weekly_returns_aud, bond_au_weekly_return)
-colnames(cor_aud_weekly) = c("AU.weekly")
-cor_usd_weekly = cor(weekly_returns_usd, bond_us_weekly_return)
-colnames(cor_usd_weekly) = c("US.weekly")
+#cor_aud_weekly = cor(weekly_returns_aud, bond_au_weekly_return)
+#colnames(cor_aud_weekly) = c("AU.weekly")
+#cor_usd_weekly = cor(weekly_returns_usd, bond_us_weekly_return)
+#colnames(cor_usd_weekly) = c("US.weekly")
 
-df = merge(cor_aud_monthly, cor_aud_weekly, by = "row.names")
-rownames(df) = df[, 1] # there must be a better way...
-df = df[, -1]
+#df = merge(cor_aud_monthly, cor_aud_weekly, by = "row.names")
+#rownames(df) = df[, 1] # there must be a better way...
+#df = df[, -1]
 
-df = merge(df, cor_usd_monthly, by = "row.names")
-rownames(df) = df[, 1]
-df = df[, -1]
+#df = merge(df, cor_usd_monthly, by = "row.names")
+#rownames(df) = df[, 1]
+#df = df[, -1]
 
-df = merge(df, cor_usd_weekly, by = "row.names")
-rownames(df) = df[, 1]
-df = df[, -1]
+#df = merge(df, cor_usd_weekly, by = "row.names")
+#rownames(df) = df[, 1]
+#df = df[, -1]
 
-write.csv(df, paste("C://Temp//", "Bond-like equities in ", fund_code, ".csv", sep = ""))
+#write.csv(df, paste("C://Temp//", "Bond-like equities in ", fund_code, ".csv", sep = ""))
 
-bond_us_monthly_return
+#bond_us_monthly_return
 
-##########################################################################
-## How are global portfolios (GLOB & GLUF) correlated to US Bond Index? ##
-##########################################################################
+###########################################################################
+### How are global portfolios (GLOB & GLUF) correlated to US Bond Index? ##
+###########################################################################
 
-# How to format upon importing according to http://stackoverflow.com/questions/13022299/specify-custom-date-format-for-colclasses-argument-in-read-table-read-csv
-setClass('myDate')
-setAs("character", "myDate", function(from) as_date(as.Date(from, format = "%d/%m/%Y")))
-raw_data = read.csv("C://Temp//PCGLOB.csv", colClasses = c('myDate', 'numeric'))
-# raw_data[["Business_Date"]] = lapply(raw_data$Date, previous_business_date_if_weekend) # Note that I can't figure out how to add a "previous business day" column, so I've fudged the data.
+## How to format upon importing according to http://stackoverflow.com/questions/13022299/specify-custom-date-format-for-colclasses-argument-in-read-table-read-csv
+#setClass('myDate')
+#setAs("character", "myDate", function(from) as_date(as.Date(from, format = "%d/%m/%Y")))
+#raw_data = read.csv("C://Temp//PCGLOB.csv", colClasses = c('myDate', 'numeric'))
+## raw_data[["Business_Date"]] = lapply(raw_data$Date, previous_business_date_if_weekend) # Note that I can't figure out how to add a "previous business day" column, so I've fudged the data.
 
-pcglob_index = as.xts(raw_data[, 2], order.by = raw_data[, 1])
-colnames(pcglob_index) = c("PCGLOB")
+#pcglob_index = as.xts(raw_data[, 2], order.by = raw_data[, 1])
+#colnames(pcglob_index) = c("PCGLOB")
 
-pcglob_index = pcglob_index["2012-02-27/"]
+#pcglob_index = pcglob_index["2012-02-27/"]
 
-pcglob_index_in_USD = pcglob_index * fx_rates
-head(pcglob_index)
-head(pcglob_index_in_USD)
+#pcglob_index_in_USD = pcglob_index * fx_rates
+#head(pcglob_index)
+#head(pcglob_index_in_USD)
 
-pcglob_index_in_USD_returns = monthlyReturn(pcglob_index_in_USD)
+#pcglob_index_in_USD_returns = monthlyReturn(pcglob_index_in_USD)
 
-cor(pcglob_index_in_USD_returns, bond_us_monthly_return)
+#cor(pcglob_index_in_USD_returns, bond_us_monthly_return)
 
-# Check using Excel
-write.zoo(pcglob_index_in_USD_returns, "C://Temp//pcglob_index_in_USD_returns.csv")
-write.zoo(bond_us_monthly_return, "C://Temp//bond_us_monthly_return.csv")
+## Check using Excel
+#write.zoo(pcglob_index_in_USD_returns, "C://Temp//pcglob_index_in_USD_returns.csv")
+#write.zoo(bond_us_monthly_return, "C://Temp//bond_us_monthly_return.csv")
 
-# Conclusion:  the 5-year monthly returns of PCGLOB (in USD) have a -6% (-0.06) correlation 
-#              with the US Treasury Bond Index as of 31 Dec 2017.
+## Conclusion:  the 5-year monthly returns of PCGLOB (in USD) have a -6% (-0.06) correlation 
+##              with the US Treasury Bond Index as of 31 Dec 2017.
 
-# Now try PCGLUF (in local currency) vs the same index
-raw_data = read.csv("C://Temp//PCGLUF.csv", colClasses = c('myDate', 'numeric'))
-pcgluf_index = as.xts(raw_data[, 2], order.by = raw_data[, 1])
-colnames(pcgluf_index) = c("PCGLUF")
+## Now try PCGLUF (in local currency) vs the same index
+#raw_data = read.csv("C://Temp//PCGLUF.csv", colClasses = c('myDate', 'numeric'))
+#pcgluf_index = as.xts(raw_data[, 2], order.by = raw_data[, 1])
+#colnames(pcgluf_index) = c("PCGLUF")
 
-pcgluf_index = pcgluf_index["2012-02-27/"]
+#pcgluf_index = pcgluf_index["2012-02-27/"]
 
-head(pcgluf_index)
+#head(pcgluf_index)
 
-pcgluf_index_returns = monthlyReturn(pcgluf_index)
+#pcgluf_index_returns = monthlyReturn(pcgluf_index)
 
-x_pcgluf_index_returns = pcgluf_index_returns["2012-03-30:2017-12-30"]
-x_bond_us_monthly_return = bond_us_monthly_return["2012-03-30:2017-12-30"]
+#x_pcgluf_index_returns = pcgluf_index_returns["2012-03-30:2017-12-30"]
+#x_bond_us_monthly_return = bond_us_monthly_return["2012-03-30:2017-12-30"]
 
-cor(x_pcgluf_index_returns, x_bond_us_monthly_return, use = "complete.obs", method = "pearson")
+#cor(x_pcgluf_index_returns, x_bond_us_monthly_return, use = "complete.obs", method = "pearson")
 
-# Check using Excel
-write.zoo(x_pcgluf_index_returns, "C://Temp//pcgluf_index_returns.csv")
-write.zoo(x_bond_us_monthly_return, "C://Temp//bond_us_monthly_return.csv")
+## Check using Excel
+#write.zoo(x_pcgluf_index_returns, "C://Temp//pcgluf_index_returns.csv")
+#write.zoo(x_bond_us_monthly_return, "C://Temp//bond_us_monthly_return.csv")
 
-?cor
+#?cor
